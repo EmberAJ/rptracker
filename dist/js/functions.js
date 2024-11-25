ed0feaf274af70a37749d9882855f69623e82f90
Dawn Eggleton <dawn.eggleton@Mac.ht.home>
2024-11-24 22:07:44 -0400

    fix apostrophe issue

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -840,7 +840,7 @@ function submitPartner(form) {
         let name = character.value.trim().toLowerCase();
         let id = character.closest('.row').querySelector('#charId').value.trim();
         characterList.push({
-            name: name,
+            name: name.replaceAll(`'`, `&apos;`),
             id: id,
         });
     });
@@ -1022,7 +1022,7 @@ function submitThread(form) {
     let featuring = [];
     featuredRows.forEach(row => {
         featuring.push({
-            name: row.querySelector('#character').options[row.querySelector('#character').selectedIndex].innerText.trim().toLowerCase(),
+            name: row.querySelector('#character').options[row.querySelector('#character').selectedIndex].innerText.trim().toLowerCase().replaceAll(`'`, `&apos;`),
             id: row.querySelector('#character').options[row.querySelector('#character').selectedIndex].value.trim(),
             writer: row.querySelector('#partner').options[row.querySelector('#partner').selectedIndex].innerText.trim().toLowerCase(),
             writerId: row.querySelector('#partner').options[row.querySelector('#partner').selectedIndex].value.trim(),
@@ -1036,7 +1036,7 @@ function submitThread(form) {
         Status: status,
         Title: title,
         Character: JSON.stringify({
-            name: character.innerText.trim().toLowerCase(),
+            name: character.innerText.trim().toLowerCase().replaceAll(`'`, `&apos;`),
             id: character.value.trim().toLowerCase(),
         }),
         Featuring: JSON.stringify(featuring),
@@ -1082,7 +1082,7 @@ function updatePartner(form) {
         let name = character.value.trim().toLowerCase();
         let id = character.closest('.row').querySelector('#charId').value.trim();
         characterList.push({
-            name: name,
+            name: name.replaceAll(`'`, `&apos;`),
             id: id,
         });
     });
@@ -1813,7 +1813,7 @@ function changeStatus(e) {
             SubmissionType: 'thread-status',
             ThreadID: e.dataset.id,
             Site: e.dataset.site,
-            Character: e.dataset.character,
+            Character: e.dataset.character.replaceAll(`'`, `&apos;`),
             Status: 'theirs'
         }, thread);
     } else if(e.dataset.status === 'theirs') {
@@ -1824,7 +1824,7 @@ function changeStatus(e) {
             SubmissionType: 'thread-status',
             ThreadID: e.dataset.id,
             Site: e.dataset.site,
-            Character: e.dataset.character,
+            Character: e.dataset.character.replaceAll(`'`, `&apos;`),
             Status: 'mine'
         }, thread);
     }
@@ -1837,7 +1837,7 @@ function markComplete(e) {
         SubmissionType: 'thread-status',
         ThreadID: e.dataset.id,
         Site: e.dataset.site,
-        Character: e.dataset.character,
+        Character: e.dataset.character.replaceAll(`'`, `&apos;`),
         Status: 'complete'
     }, thread, null, 'complete');
 }
@@ -1849,7 +1849,7 @@ function markArchived(e) {
         SubmissionType: 'thread-status',
         ThreadID: e.dataset.id,
         Site: e.dataset.site,
-        Character: e.dataset.character,
+        Character: e.dataset.character.replaceAll(`'`, `&apos;`),
         Status: 'archived'
     }, thread, null, 'archived');
 }
@@ -1875,7 +1875,7 @@ function formatThread(thread) {
         <button onClick="markComplete(this)" data-id="${thread.id}" data-site="${thread.site.Site}" data-character='${JSON.stringify(thread.character)}' title="Mark Complete"><i class="fa-regular fa-badge-check"></i><i class="fa-solid fa-spinner fa-spin"></i></button>
         <button onClick="markArchived(this)" data-id="${thread.id}" data-site="${thread.site.Site}" data-character='${JSON.stringify(thread.character)}' title="Archive"><i class="fa-regular fa-trash"></i><i class="fa-solid fa-spinner fa-spin"></i></button>`;
     } else if (thread.status !== 'archived') {
-        buttons = `<div class="icon" title="${thread.type}"></div><button onClick="markArchived(this)" data-id="${thread.id}" data-site="${thread.site.Site}" data-character='${JSON.stringify(thread.character)}' title="Archive"><i class="fa-regular fa-trash"></i><i class="fa-solid fa-spinner fa-spin"></i></button>`;
+        buttons = `<div class="icon" title="${thread.type}"></div><button onClick="markArchived(this)" data-id="${thread.id}" data-site="${thread.site.Site}" data-character='${JSON.stringify(thread.character).replaceAll(`'`, `&apos;`)}' title="Archive"><i class="fa-regular fa-trash"></i><i class="fa-solid fa-spinner fa-spin"></i></button>`;
     } else {
         buttons = `<div class="icon" title="${thread.type}"></div>`;
     }
